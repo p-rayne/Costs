58170a843d3fa5da4b790484f7c46046af6470a3
p-rayne <marlinaqua@gmail.com>
2023-03-30 12:26:43 +0300

    added route "category"

--- app/costs/urls.py
+++ app/costs/urls.py
@@ -3,4 +3,5 @@ from app.costs import viewsets
 
 router = routers.DefaultRouter()
 router.register(r"costs", viewsets.CostViewSet)
+router.register(r"category", viewsets.CategoryViewSet)
 urlpatterns = router.urls
