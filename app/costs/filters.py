bc7aea11ddcd4e2b4d6f4f288effab8415359ffa
p-rayne <marlinaqua@gmail.com>
2023-03-30 12:23:28 +0300

    added category and date filters

--- app/costs/filters.py
+++ app/costs/filters.py
@@ -1,10 +1,26 @@
-from rest_framework import filters
+from rest_framework.filters import BaseFilterBackend
+from django_filters import rest_framework as filters
+
+from .models import Cost
 
 
-class IsOwnerFilterBackend(filters.BaseFilterBackend):
+class IsOwnerFilterBackend(BaseFilterBackend):
     """
     Filter that only allows users to see their own objects.
     """
 
     def filter_queryset(self, request, queryset, view):
         return queryset.filter(owner=request.user)
+
+
+class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
+
+class CostFilter(filters.FilterSet):
+    category = CharFilterInFilter(field_name="category__name", lookup_expr="in")
+    date = filters.DateRangeFilter(field_name="date")
+
+    class Meta:
+        model = Cost
+        fields = ["category", "date"]
