66fbfad031b6166e5f7fb38fd46db9511b4f6865
p-rayne <marlinaqua@gmail.com>
2023-04-07 17:31:03 +0300

    added db_index

--- app/costs/models.py
+++ app/costs/models.py
@@ -19,12 +19,14 @@ class Category(models.Model):
 class Cost(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    owner = models.ForeignKey(User, related_name="costs", on_delete=models.CASCADE)
+    owner = models.ForeignKey(
+        User, related_name="costs", on_delete=models.CASCADE, db_index=True
+    )
     category = models.ForeignKey(
-        Category, related_name="costs", on_delete=models.CASCADE
+        Category, related_name="costs", on_delete=models.CASCADE, db_index=True
     )
     value = models.DecimalField(max_digits=11, decimal_places=2)
-    date = models.DateField()
+    date = models.DateField(db_index=True)
     created_at = models.DateField(auto_now_add=True)
     note = models.CharField(max_length=255, null=True, blank=True)
 
