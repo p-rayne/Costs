3a9800587c51a8b02c8529bd2450e364e6ac9430
p-rayne <marlinaqua@gmail.com>
2023-04-07 17:32:06 +0300

    refactoring, added @action by_date

--- app/costs/viewsets.py
+++ app/costs/viewsets.py
@@ -1,20 +1,19 @@
-from django.db import models
-from rest_framework import viewsets
+from django.db.models import Sum, F
+from rest_framework import viewsets, filters
 from rest_framework.permissions import IsAuthenticated
-from rest_framework import filters
-
-# from django_filters.rest_framework import filters
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from datetime import datetime
 from django_filters.rest_framework import DjangoFilterBackend
 from app.costs import serializers
 from app.costs.models import Cost, Category
-from app.costs.filters import IsOwnerFilterBackend, CostFilter
+from app.costs.filters import CostFilter
 
 
 class CostViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.CostSerializer
     queryset = Cost.objects.all()
     filter_backends = [
-        IsOwnerFilterBackend,
         DjangoFilterBackend,
         filters.OrderingFilter,
     ]
@@ -23,24 +22,38 @@ class CostViewSet(viewsets.ModelViewSet):
     ordering_fields = ["date", "value"]
     ordering = ["-date"]
 
+    def get_queryset(self):
+        return self.queryset.filter(owner=self.request.user).annotate(
+            category_name=F("category__name")
+        )
+
+    @action(detail=False)
+    def by_date(self, request):
+        """
+        Retrieve costs by date range.
+        """
+        start_date = request.query_params.get("start_date")
+        end_date = request.query_params.get("end_date")
+
+        if not (start_date and end_date):
+            return Response({"error": "Please provide start_date and end_date"})
+
+        start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+        end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+
+        queryset = self.get_queryset().filter(date__range=(start_date, end_date))
+        serializer = self.get_serializer(queryset, many=True)
+
+        return Response(serializer.data)
+
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     permission_classes = [IsAuthenticated]
-
-    def get_serializer_class(self):
-        if self.action in ["list", "retrieve"]:
-            self.serializer_class = serializers.ListRetrieveCategorySerializer
-        else:
-            self.serializer_class = serializers.CreateUpdateDeleteCategorySerializer
-        return self.serializer_class
+    serializer_class = serializers.CategorySerializer
 
     def get_queryset(self):
-        user = self.request.user
-        if self.action in ["list", "retrieve"]:
-            queryset = Category.objects.filter(owner=user).annotate(
-                total_spent=models.Sum(models.F("costs__value"))
-            )
-        else:
-            queryset = Category.objects.filter(owner=user)
+        queryset = self.queryset.filter(owner=self.request.user).annotate(
+            total_cost=Sum("costs__value")
+        )
         return queryset
