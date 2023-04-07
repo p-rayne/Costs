1390cb87e2cd3ae14d891c0730a87ff910ccb6f6
p-rayne <marlinaqua@gmail.com>
2023-04-07 17:30:44 +0300

    delete IsOwnerFilterBackend filter

--- app/costs/filters.py
+++ app/costs/filters.py
@@ -1,16 +1,6 @@
-from rest_framework.filters import BaseFilterBackend
 from django_filters import rest_framework as filters
 
 from .models import Cost
-
-
-class IsOwnerFilterBackend(BaseFilterBackend):
-    """
-    Filter that only allows users to see their own objects.
-    """
-
-    def filter_queryset(self, request, queryset, view):
-        return queryset.filter(owner=request.user)
 
 
 class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
