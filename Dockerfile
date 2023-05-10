d4d5a045cb82c0ae25024f44b10cf43c7a89aa73
p-rayne <marlinaqua@gmail.com>
2023-05-10 14:34:18 +0300

    added dotenv, refactoring.

--- Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM python:3.10-slim
-ENV PYTHONDONTWRITEBYTECODE=1
-ENV PYTHONUNBUFFERED=1
-WORKDIR /code
-COPY requirements.txt /code/
-RUN pip install -r requirements.txt
-COPY . /code/
-
--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,10 @@
+import os
 from pathlib import Path
 from datetime import timedelta
+
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -9,12 +14,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-c1%rv#1$_dym^4fn$krzt^_m3he_y68huo%b+!oa(xfwvo9sr%"
+SECRET_KEY = os.getenv(
+    "SECRET_KEY",
+    default="django-insecure-c1%rv#1$_dym^4fn$krzt^_m3he_y68huo%b+!oa(xfwvo9sr%",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default=1)
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", default="localhost" "127.0.0.1").split(" ")
 
 
 # Application definition
